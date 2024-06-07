127a8745be97c6ac2497069be70a9b914b10ed35
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-06-07 08:23:53 +0200

    Projekt neu erstellt https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.3.0&packaging=jar&jvmVersion=17&groupId=de.eldecker.dhbw.spring&artifactId=bcrypter&name=Bcrypter-CLI&description=interaktives%20CLI-Programm%20f%C3%BCr%20Bcrypt&packageName=de.eldecker.dhbw.spring.bcrypter&dependencies=spring-shell

---
+++ src/main/java/de/eldecker/dhbw/spring/bcrypter/BcrypterCliApplication.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.bcrypter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcrypterCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcrypterCliApplication.class, args);
+	}
+
+}
